7cda4d0b166da134dc257117c34b67bea1206853
Amit Kumar <amit.kumar4@tollgroup.com>
2022-08-26 17:50:52 +1000

    helloWorld storm app

---
+++ basic-storm-sample/src/main/java/examples/ex1_HelloWorld/TopologyMain.java
@@ -0,0 +1,11 @@
+package examples.ex1_HelloWorld;
+
+
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.topology.TopologyBuilder;
+
+
+public class TopologyMain {
+
+}
---
+++ basic-storm-sample/src/main/java/examples/ex1_HelloWorld/myFirstBolt.java
@@ -0,0 +1,13 @@
+package examples.ex1_HelloWorld;
+
+
+import org.apache.storm.topology.BasicOutputCollector;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseBasicBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+
+public class myFirstBolt extends BaseBasicBolt {
+
+}
