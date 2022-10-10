edee98b6c17533dcf2a82594379314a698da7f0f
amitkumar1278 <amitkumar12788@gmail.com>
2022-10-10 18:32:32 +1100

    updated instruction

--- basic-storm-sample/src/main/java/examples/ex1_HelloWorld/TopologyMain.java
+++ basic-storm-sample/src/main/java/examples/ex1_HelloWorld/TopologyMain.java
@@ -6,30 +6,64 @@ import org.apache.storm.LocalCluster;
 import org.apache.storm.thrift.TException;
 import org.apache.storm.topology.TopologyBuilder;
 
-
+/**
+ * We're done with setting up our spot and bolt. Now let's go ahead and use them to build our topology and then submit that topology to a clustered in the local board.
+ * We'll set up a class called TopologyMain, which will be running, so we'll put our code in the main method of this class.
+ *
+ * Then within this class, the first thing we need to do is to build our topology.
+ * Building a topology basically means that you will be adding all the spout and bolts which make up the topology and set up the connections between those components.
+ *
+ */
 public class TopologyMain {
     public static void main(String[] args) throws Exception {
 
-        //Build Topology
+        /**  Build Topology: First, we'll set up our topology builder and use it to specify the components of the topology. later we will call the create topology method of this builder to actually build the topology.         */
         TopologyBuilder builder = new TopologyBuilder();
+
+        /**  Next, we use the setSpout method to add a spout to this builder. to add this spout. We need to specify the name of the spout and pass the spout object that represents that spout.
+         *  The spot object is created using the myFirstSpout class that we set up earlier, And we've also set the name of this spout using a string, "My first spout".     */
         builder.setSpout("My-First-Spout", new myFirstSpout());
+
+        /** Just as we added a spout to the topology builder, we need to add a bolt. They do this using the setBolt method.
+         *  to the setBolt method, we passed the name of the Bolt and the bolt object itself, which is created using the myFirstBolt class. once the Bolt is set up, we need to connect this spout and the bolt.
+         *  in storm exchange of data is called grouping and shuffle grouping is the default methodology for exchanging data between components, so to simply connect the spout and the bolt, We use the shuffle grouping method and we specify which spout the bolt is connected to.
+         *  the string "My-First-Spout" refers to that name of this spout Within this topology.         */
         builder.setBolt("My-First-Bolt", new myFirstBolt()).shuffleGrouping("My-First-Spout");
 
-        //Configuration
+        /** Configuration: Next, we need to set up the configuration for this topology. We will use this config object to set parameters that will be true for the topology overall that will apply to all the components and the topology. */
         Config conf = new Config();
+        /** One of the parameters that we have explicitly set here, is the set debug true parameter. By setting this parameter, we'll be able to see that the tuples that are being emitted by the spout and the bolt in the console log when we run this topology. */
         conf.setDebug(true);
 
 
-        //Submit Topology to cluster
+        /** Submit Topology to cluster: Now we need to actually submit this topology to a cluster.
+
+         * The local cluster will create a cluster which will run in the local mode, and we can submit our topology that we set up earlier to this cluster, then that topology will run on the cluster until the cluster shuts down.
+         * Once this cluster is set up, we need to actually submit our topology to this cluster, to submit our topology will simply use the submitTopology method of the cluster.
+         * So this method we need to pass the topology name the topology configuration and the topology object itself, which we set up using the builder.createTopology() method.
+         *
+         * Once we submit the topology, it will start running, and it will continue running indefinitely. So we'll just let the thread run for a limited amount of time and then shut down the cluster.
+         *
+         */
         LocalCluster cluster = new LocalCluster();
         try{
             System.out.println("Submitting topology ");
             cluster.submitTopology("My-First-Topology", conf, builder.createTopology());
             System.out.println("Submission done");
             Thread.sleep(1000);
+        } finally{
+            cluster.shutdown();
         }
 
-        finally{
-            cluster.shutdown();}
+        /**
+         * Let's go ahead and run this to see the results of the topology running. The log will be quite robust with a lot of detail.
+         *
+         * So you just need to look for statements that say "Emitting" to see what is being emitted by each component.
+         * In log we can see what was emitted by the spout, and you can see that it is a consecutive list of integers.
+         * This list of integers is passed on to the bolt and the bolt converts them into even numbers, so each integer gets into the bolt, it is multiplied by two.
+         * Therefore, when you look at the bolt's consecutive output, you will see that they are Consecutive even numbers. for instance, as an example, if We have 362, followed by 364, followed by 366, then 368 and so on.
+         *
+         * here We've successfully created a simple topology that auto generates integer and then converts them to even numbers.
+         */
     }
 }
--- basic-storm-sample/src/main/java/examples/ex1_HelloWorld/myFirstBolt.java
+++ basic-storm-sample/src/main/java/examples/ex1_HelloWorld/myFirstBolt.java
@@ -9,16 +9,59 @@ import org.apache.storm.tuple.Tuple;
 import org.apache.storm.tuple.Values;
 
 /**
- * to implement BaseBasicBolt we have to implement 3 methods.
+ * 1. We've set up myFirstSpout of our Hello World Topology, Now let's move on and see what's involved in setting up the Bolt.
+ *      The bolt will be basically a class called myFirstBolt that extends the BaseBasicBolt abstract class, and to implement this class, we need to implement three methods.
+ *          i. execute
+ *          ii. declareOutputFields
+ *          iii. cleanup
+ *
+ * 5. once you implement these three methods, the myFirstBolt class will be setup.
+ *      Within this class will implement the logic that will take any integers which are coming into the bolt and multiply them by two before sending them on.
+ *      This class needs to extend the BaseBasicBolt class, so that's what we have done here, and we need to implement the three methods execute, declareOutputFields and cleanup to set up this class.
+ *      Let's start off with the execute method, where the actual logic of taking the input and multiplying by two happens.
  */
 public class myFirstBolt extends BaseBasicBolt {
+
+    /**
+     * 2. execute() method is the method where all the action happens, Here you will define the transformation logic that needs to happen inside this bolt.
+     * whenever there is a tuple that has been passed on from a previous element and that tuple needs to be processed by this bolt, Then the execute method is called.
+     * Within the execute method, You will put in all the logic, to process, the input tuple, and then you will use the collector to pass on the output to the next component, just the way we did in the spout.
+     *
+     * 6. within this method, We just have one line and this line is doing a bunch of things, So let's go through that in detail.
+     *
+     * @param input
+     * @param collector
+     */
     public void execute(Tuple input,BasicOutputCollector collector) {
+
+        /**
+         *  7. First, we are taking our input tuple, and from that input tuple we are getting the first value that is present in the tuple and casting it to integer.
+         *          a tuple is actually like an ordered map So the values in the tuple will be in the same order as you sent them from the previous component.
+         *          In the previous component, we sent just one value, which was an integer to the bolt. So we just need to get the value at the first index, which is zero, and we need to cast it to an integer since it will be sent as just an object.
+         *          You can do this by using the get value of method and explicitly casting that object to an integer or use the built in method for the tuple call getInteger, which will make the value at the zero index and cast it to integer automatically.
+         *          We take that value and multiplied by two, Then we just create a new values object and use the Emit method to pass on that object to the next component. So this will send a new tuple on to the next component, which contains the input integer multiplied by two that completes our execute method.
+         */
+
         collector.emit(new Values(input.getInteger(0)*2));
     }
 
+    /**
+     *  3. Every storm component, whether it's a spout or a bolt, has a fixed schema for the output tuple and that schema or the list of fields that would be emitted as part of the tuple needs to be declared for that component. We will declare that in the declared output fields method.
+     *
+     *  8. Now we can go ahead and declare the schema of the output tuples in the declared output fields This is pretty straightforward.
+     *
+     * @param declarer
+     */
     public void declareOutputFields(OutputFieldsDeclarer declarer) {
+
+        // We do this exactly as we did last time (in spout).
         declarer.declare(new Fields("field"));
     }
 
-    public void cleanup() {}
+    /**
+     * 4. This method is called whenever the cluster is shut down to do cleanup actions, such as closing a database connection, closing a file where you are writing the data and so on. For now will be leaving this method empty because we don't want any specific actions in our hello world topology When the bolt is shut down.
+     */
+    public void cleanup() {
+        // we have the clean up method, which we leave empty for now because we don't really have any actions that need to happen when the cluster shuts down.
+    }
 }
